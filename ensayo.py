d5469f1043738f11edd891720a2fcc96411e5bdc
jkdeveloper <mejor0493@gmail.com>
2017-04-08 00:58:22 -0400

    vigia merwebo

--- ensayo.py
+++ ensayo.py
@@ -2,6 +2,10 @@ vigia = [10, 9, 10]
 
 for cartas in vigia:
 	if vigia.count(cartas) == 2:
-		los_iguales = vigia.pop(vigia.index(cartas))
-		residuo= vigia.remove(los_iguales)
-		print(vigia)
+		iguales= vigia.pop(vigia.index(cartas))
+		vigia.remove(iguales)
+		residuo= vigia.pop()
+		if residuo + 1== iguales:
+			print("ESTA MIELDA ES VIGIA menor")
+		elif iguales + 1== residuo:
+			print("ESTA MIELDA ES VIGIA mayor")

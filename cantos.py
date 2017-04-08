4e9b17bcaf3ba3d8e33fc2b45b1d1d91d2bb93ce
Francisco <andrewplanet@gmail.com>
2017-04-07 23:31:03 -0400

    Agregada canto Ronda

--- cantos.py
+++ cantos.py
@@ -6,4 +6,23 @@ def casaGrande(mano):
 		return True
 	return False
 
-print(casaGrande(casa_grande))
+# print(casaGrande(casa_grande))
+
+ronda_mano = [10, 3, 10]
+
+def ronda(mano):
+	for numero in mano:
+		if mano.count(numero) == 2:
+			los_iguales = mano.pop(mano.index(numero))
+			if 1 <= los_iguales <= 7:
+				print("1 punto bicho")
+			elif los_iguales == 10:
+				print("2 puntos mano")
+			elif los_iguales == 11:
+				print("3 puntos manuscrito")
+			else:
+				print("4 puntos manofactura")
+			return True
+	return False
+
+print(ronda(ronda_mano))

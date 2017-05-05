1831f36a94975105dc78cdc37ec80b1456abe106
jkdeveloper <mejor0493@gmail.com>
2017-05-04 20:50:57 -0400

    casa chica, patrulla, registro

--- cantos.py
+++ cantos.py
@@ -10,6 +10,30 @@ def casaGrande(mano):
 
 # print(casaGrande(casa_grande))
 
+#CASA CHICA
+
+casa_chica= [12, 1, 12]
+
+def casaChica(mano):
+	if [1, 1, 12] == sorted(mano):
+		print("Casa Chica")
+		return True
+	return False
+
+# print(casaChica(casa_chica))
+
+#REGISTRO
+
+registro= [15, 11, 1]
+
+def Registro(mano):
+	if [1, 11, 12] == sorted(mano):
+		print("Registro")
+		return True
+	return False
+
+#print(Registro(registro))
+
 #RONDA
 
 ronda_mano = [10, 3, 10]
@@ -61,3 +85,18 @@ def vigia(mano):
 	return False
 
 # print(vigia(vigia_mano))
+
+#PATRULLA
+cards= [7, 10, 11]
+hand= sorted(cards)
+
+def Patrulla(mano):
+	if hand[0] == 6 and hand[1] + 3== hand[2]:
+		return("Es Patrulla 6")
+	elif hand[0]== 7 and hand[1]+ 1== hand[2]:
+		return("patrulla 7")		
+	elif hand[0] + 1 == hand[1] and hand[1] + 1== hand[2]:
+		return("Esto es patrulla")
+	else:
+		return False	
+#print(Patrulla(hand))
